4f6de4d2b8402ef8ddca05dabadab036d5a481a3
Imam Chishty <imam.chishty@emc.com>
2016-10-10 14:05:53 +0400

    Fixed bug!

--- src/main/resources/archetype-resources/__artifactId__-rest/src/test/java/__packageInPathFormat__/rest/PingControllerIntegrationTest.java
+++ src/main/resources/archetype-resources/__artifactId__-rest/src/test/java/__packageInPathFormat__/rest/PingControllerIntegrationTest.java
@@ -1,7 +1,6 @@
 package ${package}.rest;
 
 import ${package}.rest.constant.ApiConstants;
-import com.kungfu.panda.rest.constant.ApiConstants;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.beans.factory.annotation.Autowired;
